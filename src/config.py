9d6956057a15a7c39b31b9680cb3698fc950dd96
Eloi Sandt <eloi.sandt@campus.tu-berlin.de>
2024-05-01 11:56:40 +0200

    pulled pdm into brach, applied prettify rules
    Signed-off-by: Eloi Sandt <eloi.sandt@campus.tu-berlin.de>

--- src/config.py
+++ src/config.py
@@ -1,35 +1,39 @@
 import json
 
 class Config:
-    """
-    A class for a structured, extensible, knowledge-domain agnostic way to store user-handed targets for scrapping from various types of sources
-    """
-    #TODO: add more types of scaping targets here
-    archiveTargets = []
+    """Convert json config to python objects and vice versa.
 
-    def addArchiveTarget(self, url, keywords):
-        self.archiveTargets.append(Archives(url, keywords))
+    A class for a structured, extensible, knowledge-domain agnostic way
+    to store user-handed targets for scrapping from various types of sources.
+    """
+
+    #TODO: add more types of scaping targets here
+    archive_targets = []
+
+    def add_archive_target(self, url, keywords):
+        self.archive_targets.append(Archives(url, keywords))
 
     "Used in the JSON encoder"
-    def toDict(self):
-        return dict(archiveTargets=self.archiveTargets) # TODO: add new Scraping Variables here
+    def to_dict(self):
+        return dict(archive_targets=self.archive_targets)
+        # TODO: add new Scraping Variables here
 
-    def writeToJson(self, path):
-        jsonData = json.dumps(
+    def write_to_json(self, path):
+        json_data = json.dumps(
             self,
             sort_keys=True,
             indent=4,
-            cls=JsonEncoder)
-        with open(path, "w") as jsonFile:
-            jsonFile.write(jsonData)
+            cls=json_encoder)
+        with open(path, "w") as json_file:
+            json_file.write(json_data)
 
-    def fromJson(self, path):
-        with open(path) as jsonFile:
-            jsonData = json.load(jsonFile)
+    def from_json(self, path):
+        with open(path) as json_file:
+            json_data = json.load(json_file)
 
             # fill data fields of this class with data from json
-            jsonArchiveTargets = jsonData.get("archiveTargets")
-            for entry in jsonArchiveTargets:
+            jsonarchive_targets = json_data.get("archive_targets")
+            for entry in jsonarchive_targets:
                 self.addArchiveTarget(entry.get('urls'), entry.get('keywords'))
             #TODO: add new Scraping Objects here
 
@@ -38,12 +42,15 @@ class Config:
     #---------------------------------------------------------
 
     def __init__(self):
-    # this only serves as an example. Ideally, you would want to call the fromJason method to fill this object with data from an existing Json file
-        #self.addArchiveTarget(["http://arxiv.org","https://pubmed.ncbi.nlm.nih.gov/"] , ["nutrition", "health", "food as medicine"])
+        # this only serves as an example. Ideally, you would want to call the fromJason
+        # method to fill this object with data from an existing Json file
+
+        #self.addArchiveTarget(["http://arxiv.org","https://pubmed.ncbi.nlm.nih.gov/"]
+        #, ["nutrition", "health", "food as medicine"])
         pass
     def __repr__(self):
-        fmt_string = f"Config(archiveTargets: ["
-        for entry in self.archiveTargets:
+        fmt_string = f"Config(archive_targets: ["
+        for entry in self.archive_targets:
             fmt_string += repr(entry)
         fmt_string += "])"
         return fmt_string
@@ -55,15 +62,14 @@ class Config:
 # MARK: Encoding/Decoding Classes
 #---------------------------------------------------------
 
-class JsonEncoder(json.JSONEncoder):
-    """
-    Custom encoder tells the JSON library how to encode our objects
-    """
+class JsonEncoder(json.json_encoder):
+    """Custom encoder tells the JSON library how to encode our objects."""
+
     def default(self, obj):
-        if hasattr(obj, "toDict"):
-            return obj.toDict()
+        if hasattr(obj, "to_dict"):
+            return obj.to_dict()
         else:
-            return json.JSONEncoder.default(self, obj)
+            return json.json_encoder.default(self, obj)
 
 #---------------------------------------------------------
 # MARK: Classes for Scraping Targets
@@ -72,17 +78,14 @@ class JsonEncoder(json.JSONEncoder):
 # TODO: This class serves as an example for how targets could be stored.
 # Similarly create classes for youtube, websites, podcast, ... targets
 class Archives:
-    """
-    Wrapper class for our different archives
+    """Wrapper class for our different archives."""
 
-    """
     class Archive:
+        """A website containing various papers to scrape.
 
-        """
-        A website containing various papers to scrape
         Filter papers based on keywords
-
         """
+
         #-------------------------------------------------------------
         def __init__(self, url, keywords):
             self.url = url
@@ -97,10 +100,10 @@ class Archives:
             return f"Archive Details:\nURLs: {url_str}\nKeywords: {keyword_str}"
         #------------------------------------------------------------
 
-    def getArchives():
+    def get_archives():
         return self.archives
 
-    def toDict(self):
+    def to_dict(self):
         return dict(urls=self.urls, keywords=self.keywords)
 
     #------------------------------------------------------------
@@ -118,14 +121,15 @@ class Archives:
     def __str__(self):
         urls_str = ', '.join(self.urls)
         keyword_str = ', '.join(self.keywords)
-        return f"Archives Details:\nURLs: {url_str}\nKeywords: {keyword_str}"
+        return f"Archives Details:\nURLs: {urls_str}\nKeywords: {keyword_str}"
 
     #------------------------------------------------------------
 
 # MARK: Execution
 
 config = Config()
-#config.writeToJson("config.json")
-#config.fromJson("config.json")
+#config.write_to_json
+("config.json")
+#config.from_json("config.json")
 
 
