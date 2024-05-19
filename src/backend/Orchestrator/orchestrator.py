789586d7a4399224d202674957e2373c2ad77160
Preet Vadaliya <preetvadaliya@gmail.com>
2024-05-19 20:53:56 +0200

    add: orchestrator module

---
+++ src/backend/Orchestrator/orchestrator.py
@@ -0,0 +1,16 @@
+from src.backend.Config.config import Config
+from typing import List
+from src.backend.Scrapers.BaseScraper.base_scraper import BaseScraper
+
+
+class Orchestrator:
+  def __init__(self, config: Config):
+    self._config = config
+
+  def run(self):
+    target_groups = self._config.get_all_targets()
+    all_new_target_elements: List[BaseScraper] = []
+    for target in target_groups:
+      all_new_target_elements.extend(target.get_all_new_target_elements())
+    for element in all_new_target_elements:
+      element.scrape_and_save()
