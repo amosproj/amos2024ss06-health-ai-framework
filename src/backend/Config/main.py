2f0ba81e55d74520f27540f2fbab55f060f75fec
Preet Vadaliya <preetvadaliya@gmail.com>
2024-05-20 11:07:30 +0200

    add: example of building config

--- src/backend/Config/main.py
+++ src/backend/Config/main.py
@@ -2,7 +2,7 @@ from src.backend.Config.config import Config
 from src.backend.ScrappingTarget.youtube_target import YouTubeTarget
 
 if __name__ == '__main__':
-  c = Config.from_json()
-  c.add_target(YouTubeTarget('test_url_3'))
-  print(c.to_dict())
+  c = Config()
+  c.add_target(YouTubeTarget(url='https://www.youtube.com/@NutritionFactsOrg'))
+  c.add_target(YouTubeTarget(url='https://www.youtube.com/@DrWilliamLi'))
   c.write_to_json()
