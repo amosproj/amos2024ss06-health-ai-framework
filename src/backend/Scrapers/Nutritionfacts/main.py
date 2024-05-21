e2ca519066d749fba40679135bbf4355fc180eee
Eloi Sandt <eloi.sandt@campus.tu-berlin.de>
2024-05-21 10:04:05 +0200

    adapt changes to directory structure on main branch for merge
    Signed-off-by: Eloi Sandt <eloi.sandt@campus.tu-berlin.de>

---
+++ src/backend/Scrapers/Nutritionfacts/main.py
@@ -0,0 +1,7 @@
+from nutritionfacts import scrape_blogs
+
+#TODO: ADAPT THIS TO PREET'S NEW ORCHESTRATOR PIPELINE NEXT SPRINT
+
+if __name__ == "__main__":
+    base_url = 'https://nutritionfacts.org/blog/'
+    scrape_blogs(base_url)
