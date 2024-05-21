cb45245b1ea03545c71d20c63c966ca71af2cf81
Preet Vadaliya <preetvadaliya@gmail.com>
2024-05-21 12:58:42 +0200

    change: all recipes enhance

--- src/backend/Orchestrator/main.py
+++ src/backend/Orchestrator/main.py
@@ -1,7 +1,10 @@
 from src.backend.Config.config import Config
+
 from src.backend.Orchestrator.orchestrator import Orchestrator
 
 if __name__ == '__main__':
   config = Config.from_json()
+  # config.add_target(AllRecipesTarget(search_query='india'))
+  # config.write_to_json()
   orchestrator = Orchestrator(config=config)
   orchestrator.run()
