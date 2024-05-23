8d411aa20dfcce62eecd59abe938c99bc1f23f7a
Eloi Sandt <eloi.sandt@campus.tu-berlin.de>
2024-05-23 11:28:00 +0200

    add: old pubmed code
    Signed-off-by: Eloi Sandt <eloi.sandt@campus.tu-berlin.de>

--- src/backend/Config/main.py
+++ src/backend/Config/main.py
@@ -1,12 +1,14 @@
 from src.backend.Config.config import Config
-from src.backend.ScrappingTarget.youtube_target import YouTubeTarget
-from src.backend.ScrappingTarget.archive_target import ArchiveTarget
+from src.backend.ScrappingTarget.pubmed_target import PubMedTarget
 
 if __name__ == '__main__':
   c = Config()
-  c.add_target(YouTubeTarget(url='https://www.youtube.com/@NutritionFactsOrg'))
-  c.add_target(YouTubeTarget(url='https://www.youtube.com/@DrWilliamLi'))
+  # c.add_target(YouTubeTarget(url='https://www.youtube.com/@NutritionFactsOrg'))
+  # c.add_target(YouTubeTarget(url='https://www.youtube.com/@DrWilliamLi'))
+  # c.add_target(
+  #   ArchiveTarget(keywords=['nutrition', 'health', 'food as medicine'], max_results=1)
+  # ) #TODO: reenable previous targets after testing pubmed
   c.add_target(
-    ArchiveTarget(keywords=['nutrition', 'health', 'food as medicine'], max_results=1)
+    PubMedTarget(keywords=['nutrition', 'health', 'food as medicine'], max_results=1)
   )
   c.write_to_json()
--- src/backend/ScrappingTarget/pubmed_target.py
+++ src/backend/ScrappingTarget/pubmed_target.py
@@ -5,8 +5,9 @@ from src.backend.ScrappingTarget.base_target import BaseTarget
 
 
 class PubMedTarget(BaseTarget):
-  def __init__(self):
-    pass
+  def __init__(self, keywords=[], max_results=1):
+    self.keywords = keywords
+    self.max_results = max_results  # max search query results for the set of keywords
 
   def get_all_new_target_elements(self) -> List[PubMedScraper]:
     return PubMedScraper.get_all_possible_elements(self)
