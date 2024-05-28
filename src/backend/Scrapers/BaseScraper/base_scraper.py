d07c8393847b4f0369ef4aa808a9c72982cce476
Preet Vadaliya <preetvadaliya@gmail.com>
2024-05-28 13:35:58 +0200

    fix: prevent save data if scrape data is blank
    Signed-off-by: Preet Vadaliya <preetvadaliya@gmail.com>

--- src/backend/Scrapers/BaseScraper/base_scraper.py
+++ src/backend/Scrapers/BaseScraper/base_scraper.py
@@ -30,20 +30,25 @@ class BaseScraper(metaclass=ABCMeta):
     indexes: [str] = json.load(open(self.index_file, 'r').read()).get('indexes', [])
     return self.element_id in indexes
 
-  def _save(self, raw_data: str):
-    with open(os.path.join(type(self).base_dir(), f'{self.element_id}.txt'), 'w') as file:
-      file.write(raw_data)
+  def _save(self, scrapped_dict: dict):
+    file_path = os.path.join(self.base_dir(), f'{self.element_id}.json')
+    with open(file_path, 'w') as file:
+      json.dump(scrapped_dict, file)
 
-  def scrape_and_save(self):
-    raw_data = self._scrape()
-    self._save(raw_data)
-    # Read index data from the file
-    with open(type(self).index_file(), 'r+') as file:
-      index_data = json.load(file)
-      # Update the indexes
-      indexes = index_data.get('indexes', [])
-      indexes.append(self.element_id)
-      index_data['indexes'] = indexes
-      # Write the updated index data back to the file
-      file.seek(0)
-      json.dump(index_data, file, indent=2)
+    def scrape_and_save(self):
+      scrapped_dict = self._scrape()
+      # Check if the scrapped_dict is empty
+      if not scrapped_dict:
+        print('Scrapped data is empty. Nothing to save or update.')
+        return
+      self._save(scrapped_dict)  # save scrapped json file
+      # Read index data from the file
+      with open(type(self).index_file(), 'r+') as file:
+        index_data = json.load(file)
+        # Update the indexes
+        indexes = index_data.get('indexes', [])
+        indexes.append(self.element_id)
+        index_data['indexes'] = indexes
+        # Write the updated index data back to the file
+        file.seek(0)
+        json.dump(index_data, file, indent=2)
