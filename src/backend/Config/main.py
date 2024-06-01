b9f060f1b531f77f3bda181435ffb9d1003dff89
Eloi <70521773+eloinoel@users.noreply.github.com>
2024-06-01 21:44:40 +0200

    126 patch for the pubmed scraper pdf downloading problems (#146)
    * fix: pubmed error for publication date metadata
    
    Signed-off-by: Eloi Sandt <eloi.sandt@campus.tu-berlin.de>
    
    * fix: pubmed pdf download for certain papers works. If not, only metadata will be stored. Some papers  might be hidden behind paywall or miscellaneous links
    
    Signed-off-by: Eloi Sandt <eloi.sandt@campus.tu-berlin.de>
    
    * finished testing pubmed with orchestrator
    
    Signed-off-by: Eloi Sandt <eloi.sandt@campus.tu-berlin.de>
    
    ---------
    
    Signed-off-by: Eloi Sandt <eloi.sandt@campus.tu-berlin.de>

--- src/backend/Config/main.py
+++ src/backend/Config/main.py
@@ -1,7 +1,5 @@
 from src.backend.Config.config import Config
 
-from src.backend.ScrappingTarget.archive_target import ArchiveTarget
-from src.backend.ScrappingTarget.podcast_target import PodcastTarget
 from src.backend.ScrappingTarget.pubmed_target import PubMedTarget
 
 if __name__ == '__main__':
@@ -9,10 +7,10 @@ if __name__ == '__main__':
     # c.add_target(YouTubeTarget(url='https://www.youtube.com/@NutritionFactsOrg'))
     # c.add_target(YouTubeTarget(url='https://www.youtube.com/@DrWilliamLi'))
 
-    keys = ['nutrition', 'health', 'food as medicine']
-    c.add_target(ArchiveTarget(keywords=keys, max_results=1))
-    url = 'https://peterattiamd.com/podcast/archive/'
-    c.add_target(PodcastTarget(url=url, num_podcasts=1))
+    # keys = ['nutrition', 'health', 'food as medicine']
+    # c.add_target(ArchiveTarget(keywords=keys, max_results=1))
+    # url = 'https://peterattiamd.com/podcast/archive/'
+    # c.add_target(PodcastTarget(url=url, num_podcasts=1))
     # c.add_target(AllRecipesTarget())
     c.add_target(PubMedTarget(keywords=['nutrition', 'health', 'food as medicine'], max_results=3))
     c.write_to_json()
--- src/backend/Scrapers/PubMed/main.py
+++ src/backend/Scrapers/PubMed/main.py
@@ -1,5 +1,21 @@
+from src.backend.Scrapers.PubMed.pub_med import PubMedScraper
+from src.backend.ScrappingTarget.pubmed_target import PubMedTarget
+
 if __name__ == '__main__':
     print('Pubmed Scraper')
-    # test = PubMedScraper("31181616")
-    # result = test._scrape()
-    # print(result)
+    scrapers = PubMedScraper.get_all_possible_elements(
+        PubMedTarget(keywords=['nutrition', 'health', 'food as medicine'], max_results=3)
+    )
+
+    print('Scraping for ids ' + repr(scrapers) + '\n\n')
+    for scraper in scrapers:
+        data = scraper._scrape()
+        for item in data.items():
+            print(item[0], end=': ')
+            if item[0] == 'text' or item[0] == 'abstract':
+                print(type(item[1]))
+            else:
+                print(item[1])
+
+            # print(item[1])
+        print('-----------------------')
