6567ddcba70f95c3a6884b80ada7f2767f0bddcd
lukas-varga <lukas.varga128@gmail.com>
2024-06-02 17:57:33 +0200

    Added main() target Co-authored-by: Manik Gupta manikg08 manik.gupta@fau.de

--- src/backend/Config/main.py
+++ src/backend/Config/main.py
@@ -1,21 +1,8 @@
 from src.backend.Config.config import Config
 
-from src.backend.ScrappingTarget.archive_target import ArchiveTarget
-from src.backend.ScrappingTarget.podcast_target import PodcastTarget
 from src.backend.ScrappingTarget.nutrition_target import NutritionTarget
-from src.backend.ScrappingTarget.pubmed_target import PubMedTarget
 
 if __name__ == '__main__':
     c = Config()
-    # c.add_target(YouTubeTarget(url='https://www.youtube.com/@NutritionFactsOrg'))
-    # c.add_target(YouTubeTarget(url='https://www.youtube.com/@DrWilliamLi'))
-
-    # keys = ['nutrition', 'health', 'food as medicine']
-    # c.add_target(ArchiveTarget(keywords=keys, max_results=1))
-    # url = 'https://peterattiamd.com/podcast/archive/'
-    # c.add_target(PodcastTarget(url=url, num_podcasts=1))
-    # c.add_target(AllRecipesTarget())
-    # pubmed = PubMedTarget(keywords=['nutrition', 'health', 'food as medicine'], max_results=3)
-    # c.add_target(pubmed)
-    c.add_target(NutritionTarget(url='https://nutritionfacts.org/blog/', max_pages=1))
+    c.add_target(NutritionTarget(max_pages=1))
     c.write_to_json()
--- src/backend/Scrapers/Nutrition/main.py
+++ src/backend/Scrapers/Nutrition/main.py
@@ -1,2 +1,6 @@
+from src.backend.ScrappingTarget.nutrition_target import NutritionTarget
+from src.backend.Orchestrator.orchestrator import Orchestrator
+
 if __name__ == '__main__':
-    print()
+    target = NutritionTarget(max_pages=1)
+    Orchestrator.run_target(target)
