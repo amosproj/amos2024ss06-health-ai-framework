63e6f5dfec1529ac16bf0e255457de68069ddc2c
Preet Vadaliya <preetvadaliya@gmail.com>
2024-06-08 19:17:17 +0200

    add: navigation deps and babel config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'],
     env: {
       prod: {
         plugins: ['react-native-paper/babel']
