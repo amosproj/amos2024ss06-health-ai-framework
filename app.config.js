03e83d806a8e0b8030a34f9f6c00c506cd6fb984
Preet Vadaliya <preetvadaliya@gmail.com>
2024-06-10 01:20:07 +0200

    add: google sign in logic

--- app.config.js
+++ app.config.js
@@ -20,7 +20,9 @@ module.exports = ({ config }) => {
         storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.FIREBASE_APP_ID,
-      }
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+      },
+      googleAuthClientId: process.env.GOOGLE_AUTH_CLIENT_ID
     }
   };
 };
--- src/frontend/screens/Landing/LogInWithGoogle.tsx
+++ src/frontend/screens/Landing/LogInWithGoogle.tsx
@@ -1,11 +1,41 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { useNavigation } from '@react-navigation/native';
+import Constants from 'expo-constants';
+import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
 import React from 'react';
 import { Button } from 'react-native-paper';
-import { GoogleIcon } from 'src/frontend/components';
+import { useAuth } from 'reactfire';
 
 export function LogInWithGoogle() {
+  const fireAuth = useAuth();
+  const { navigate } = useNavigation();
+
+  const handleLogInWithGoogle = async () => {
+    try {
+      // Configure Google Signin
+      GoogleSignin.configure({
+        // Get the web client ID from the Expo configuration
+        webClientId: Constants.expoConfig?.extra?.googleAuthClientId,
+        // We want to force the code for the refresh token
+        forceCodeForRefreshToken: true
+      });
+      // Sign in with Google
+      // This will prompt the user to choose a Google account to sign in with
+      const { idToken } = await GoogleSignin.signIn();
+      // Create a credential object using the ID token
+      // This credential object will be used to authenticate with Firebase
+      const googleCredential = GoogleAuthProvider.credential(idToken);
+      // Sign in with the credential object using Firebase
+      // This will create a Firebase user if it doesn't exist, or sign in the existing user
+      const { user: fireUser } = await signInWithCredential(fireAuth, googleCredential);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <Button mode='outlined' icon={GoogleIcon}>
-      TEST
+    <Button onPress={handleLogInWithGoogle} mode='outlined'>
+      Sign in with Google
     </Button>
   );
 }
