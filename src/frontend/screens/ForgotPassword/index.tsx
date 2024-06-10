04e1b727c8b66547a08aec2099dc1b81437a5b37
Preet Vadaliya <preetvadaliya@gmail.com>
2024-06-10 22:39:44 +0200

    feat: Implement password reset functionality
    Signed-off-by: Preet Vadaliya <preetvadaliya@gmail.com>

--- src/frontend/screens/ForgotPassword/index.tsx
+++ src/frontend/screens/ForgotPassword/index.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import React, { useCallback } from 'react';
 import { Text, View } from 'react-native';
+import { useAuth } from 'reactfire';
+
+type ForgotPasswordFormData = {
+  email: string;
+};
 
 export function ForgotPassword() {
+  const fireAuth = useAuth();
+
+  const handleForgotPassword = useCallback(
+    async (data: ForgotPasswordFormData) => {
+      const { email } = data;
+      try {
+        await sendPasswordResetEmail(fireAuth, email, {
+          url: 'https://amos-agent-framework.firebaseapp.com/__/auth/action',
+          handleCodeInApp: true,
+          iOS: {
+            bundleId: 'com.amos.ailixir'
+          },
+          android: {
+            packageName: 'com.amos.ailixir',
+            installApp: true
+          },
+          dynamicLinkDomain: 'ailixir.page.link'
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [fireAuth]
+  );
+
+  handleForgotPassword({ email: 'preetvadaliya@gmail.com' });
+
   return (
     <View>
       <Text>ForgotPassword</Text>
