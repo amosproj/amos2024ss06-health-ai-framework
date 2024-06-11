0ace23f4383bb5fc3ee4da648b49e4a92f607037
Preet Vadaliya <preetvadaliya@gmail.com>
2024-06-11 17:41:19 +0200

    feat: Add sign out functionality to Chat screen
    Signed-off-by: Preet Vadaliya <preetvadaliya@gmail.com>

--- src/frontend/screens/Chat/index.tsx
+++ src/frontend/screens/Chat/index.tsx
@@ -1,10 +1,38 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { useNavigation } from '@react-navigation/native';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import Constants from 'expo-constants';
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+import { useAuth } from 'reactfire';
+import { Screens } from 'src/frontend/helpers';
+import type { AppRoutesParams } from 'src/frontend/routes';
 
 export function Chat() {
+  const fireAuth = useAuth();
+  const { navigate } = useNavigation<NativeStackNavigationProp<AppRoutesParams>>();
+
+  const handleSignOut = async () => {
+    try {
+      GoogleSignin.configure({
+        // Get the web client ID from the Expo configuration
+        webClientId: Constants.expoConfig?.extra?.googleAuthClientId,
+        // We want to force the code for the refresh token
+        forceCodeForRefreshToken: true
+      });
+      await GoogleSignin.revokeAccess();
+      await signOut(fireAuth);
+      navigate('Auth', { screen: Screens.Landing });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View>
-      <Text>Chat</Text>
+      <Button onPress={handleSignOut}>Sign Out</Button>
     </View>
   );
 }
--- src/frontend/screens/Landing/index.tsx
+++ src/frontend/screens/Landing/index.tsx
@@ -4,6 +4,7 @@ import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React, { useMemo, useRef } from 'react';
 import { View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
+import { ScreenLayout } from 'src/frontend/components';
 import { Screens } from 'src/frontend/helpers';
 import { AilixirLogo } from 'src/frontend/icons';
 import type { AppRoutesParams } from 'src/frontend/routes';
@@ -16,20 +17,20 @@ export function Landing() {
   const { navigate } = useNavigation<NativeStackNavigationProp<AppRoutesParams>>();
 
   return (
-    <View style={Style.container}>
-      <Text>{process.env.YOUTUBE_DATA_API_V3}</Text>
+    <ScreenLayout>
       <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints} index={0}>
         <BottomSheetView style={Style.sheetContainer}>
           <View style={Style.sheetHeader}>
             <AilixirLogo width={80} height={80} style={{ alignSelf: 'center' }} />
             <Text variant='titleLarge' style={Style.title}>
-              AiLixir
+              Continue to AiLixir
             </Text>
           </View>
           <View style={Style.sheetContent}>
             <LogInWithGoogle />
             <Button
               mode='contained'
+              style={{ marginTop: 16 }}
               onPress={() => navigate('Auth', { screen: Screens.SignUp })}
               contentStyle={{
                 height: 48
@@ -38,9 +39,20 @@ export function Landing() {
             >
               Sign up with Email
             </Button>
+            <Button
+              mode='contained'
+              style={{ marginTop: 16 }}
+              onPress={() => navigate('Auth', { screen: Screens.LogIn })}
+              contentStyle={{
+                height: 48
+              }}
+              labelStyle={{ fontSize: 18 }}
+            >
+              Log in with Email
+            </Button>
           </View>
         </BottomSheetView>
       </BottomSheet>
-    </View>
+    </ScreenLayout>
   );
 }
