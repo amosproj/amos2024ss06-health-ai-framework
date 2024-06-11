f22451bbb73fdae0abb6bac37bb387a45a85b170
Preet Vadaliya <preetvadaliya@gmail.com>
2024-06-11 17:40:33 +0200

    feat: Add form for password reset in ForgotPassword screen
    Signed-off-by: Preet Vadaliya <preetvadaliya@gmail.com>

--- src/frontend/screens/ForgotPassword/index.tsx
+++ src/frontend/screens/ForgotPassword/index.tsx
@@ -1,7 +1,17 @@
+import { useNavigation } from '@react-navigation/native';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { sendPasswordResetEmail } from 'firebase/auth';
-import React, { useCallback } from 'react';
-import { Text, View } from 'react-native';
+import { Formik } from 'formik';
+import React, { useCallback, useState } from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
 import { useAuth } from 'reactfire';
+import { ScreenLayout, SubmitButton, TextInput } from 'src/frontend/components';
+import { Screens } from 'src/frontend/helpers';
+import { AilixirLogo } from 'src/frontend/icons';
+import type { AppRoutesParams } from 'src/frontend/routes';
+import * as Yup from 'yup';
+import { Style } from './style';
 
 type ForgotPasswordFormData = {
   email: string;
@@ -9,10 +19,13 @@ type ForgotPasswordFormData = {
 
 export function ForgotPassword() {
   const fireAuth = useAuth();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { navigate } = useNavigation<NativeStackNavigationProp<AppRoutesParams>>();
 
   const handleForgotPassword = useCallback(
     async (data: ForgotPasswordFormData) => {
       const { email } = data;
+      setIsSubmitting(true);
       try {
         await sendPasswordResetEmail(fireAuth, email, {
           url: 'https://amos-agent-framework.firebaseapp.com/__/auth/action',
@@ -26,18 +39,42 @@ export function ForgotPassword() {
           },
           dynamicLinkDomain: 'ailixir.page.link'
         });
+        navigate('Auth', { screen: Screens.LogIn });
       } catch (error) {
         console.error(error);
       }
+      setIsSubmitting(false);
     },
     [fireAuth]
   );
 
-  handleForgotPassword({ email: 'preetvadaliya@gmail.com' });
-
   return (
-    <View>
-      <Text>ForgotPassword</Text>
-    </View>
+    <ScreenLayout>
+      <View style={Style.container}>
+        <View style={Style.header}>
+          <AilixirLogo height={80} width={80} style={{ marginBottom: 16 }} />
+          <Text variant='titleLarge'>Forgot Password</Text>
+        </View>
+        <Formik
+          initialValues={{ email: '' }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string().email().required()
+          })}
+          onSubmit={handleForgotPassword}
+          validateOnBlur={true}
+          validateOnChange={true}
+        >
+          <View style={Style.formContainer}>
+            <TextInput
+              fieldName='email'
+              label='Email'
+              keyboardType='email-address'
+              disabled={isSubmitting}
+            />
+            <SubmitButton>Send Password Reset Email</SubmitButton>
+          </View>
+        </Formik>
+      </View>
+    </ScreenLayout>
   );
 }
