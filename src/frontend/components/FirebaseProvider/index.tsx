af173a8144b5c0916f285e6974528f7208e2c5ce
Preet Vadaliya <preetvadaliya@gmail.com>
2024-06-11 17:38:50 +0200

    chore: Update FirebaseProvider to use getApps instead of type FirebaseOptions
    Signed-off-by: Preet Vadaliya <preetvadaliya@gmail.com>

--- src/frontend/components/FirebaseProvider/index.tsx
+++ src/frontend/components/FirebaseProvider/index.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants';
-import { type FirebaseOptions, initializeApp } from 'firebase/app';
+import { type FirebaseOptions, getApps, initializeApp } from 'firebase/app';
 import { initializeAuth as getAuth, getReactNativePersistence as store } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { type ReactNode, useMemo } from 'react';
@@ -13,13 +13,15 @@ type FirebaseProviderProps = {
 export function FirebaseProvider(props: FirebaseProviderProps) {
   const { children } = props;
 
-  const firebaseConfig = useMemo<FirebaseOptions>(() => Constants.expoConfig?.extra?.firebase, []);
-  const fireApp = useMemo(() => initializeApp(firebaseConfig), [firebaseConfig]);
+  const fireApp = useMemo(() => initializeApp(Constants.expoConfig?.extra?.firebase), []);
   const fireAuth = useMemo(() => getAuth(fireApp, { persistence: store(AsyncStorage) }), [fireApp]);
   const fireStore = useMemo(() => getFirestore(fireApp), [fireApp]);
 
   return (
-    <FirebaseAppProvider firebaseConfig={firebaseConfig} firebaseApp={fireApp}>
+    <FirebaseAppProvider
+      firebaseConfig={Constants.expoConfig?.extra?.firebase}
+      firebaseApp={fireApp}
+    >
       <AuthProvider sdk={fireAuth}>
         <FirestoreProvider sdk={fireStore}>{children}</FirestoreProvider>
       </AuthProvider>
