46a6eb5fb6514b8f6f8dd81fc531e3a965ce194e
Preet Vadaliya <preetvadaliya@gmail.com>
2024-06-16 00:39:18 +0200

    chore: Update text splitter utility with new chunk size and overlap values
    Signed-off-by: Preet Vadaliya <preetvadaliya@gmail.com>

--- src/backend/Utils/splitter.py
+++ src/backend/Utils/splitter.py
@@ -5,6 +5,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def get_text_chunks(text: str) -> List[str]:
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500, chunk_overlap=20, length_function=len, is_separator_regex=False
+        chunk_size=512, chunk_overlap=25, length_function=len, is_separator_regex=False
     )
     return text_splitter.split_text(text=text)
