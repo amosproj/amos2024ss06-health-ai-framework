c89420aa2eb8419f32b9e6f4b22b019aeae1dd8e
David Enescu <david.enescu@fau.de>
2024-06-18 19:23:24 +0200

    fixed bug

--- src/frontend/TO INTEGRATE - SideDrawerMenu/app/_layout.tsx
+++ src/frontend/TO INTEGRATE - SideDrawerMenu/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "expo-router";
+/*import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
@@ -7,3 +7,4 @@ export default function RootLayout() {
     </Stack>
   );
 }
+*/
