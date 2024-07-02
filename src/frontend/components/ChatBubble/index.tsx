082a2bce6e2f2e1660356c084f78a4fcd2b6509a
Eloi Sandt <eloi.sandt@campus.tu-berlin.de>
2024-07-02 17:14:19 +0200

    Fix: state with strings so selected LLM doesnt change upon refetching DB
    Signed-off-by: Eloi Sandt <eloi.sandt@campus.tu-berlin.de>

--- src/frontend/components/ChatBubble/index.tsx
+++ src/frontend/components/ChatBubble/index.tsx
@@ -13,22 +13,49 @@ type ChatBubbleProps = {
 export function ChatBubble ({message} : ChatBubbleProps) {
   const { colors } = useTheme();
 
-  const [responseIndex, setResponseIndex] = useState(0);
+  //----------------Define states etc-----------------
   const isUser = 'user' in message;
   const responses = !isUser ? Object.entries(message) : [];
-  const[llm, response] = !isUser ? responses[responseIndex] : [null, null];
 
+  if(!isUser && responses.length === 0) {
+    return <ActivityIndicator/>;
+  }
+  const [llm, setLLM] = useState(responses.length > 0 ? responses[0][0] : 'error');
+  const response = llm === 'error' ? 'error' : message[llm];
+
+  //---------------Functions for buttons----------------
   const handleNextResponse = () => {
-    const nextIndex = (responseIndex + 1) % Object.entries(message).length;
-    setResponseIndex(nextIndex);
+    const curIndex = responses.findIndex(([key, value]) => key === llm);
+    if(curIndex === -1) {
+      console.log("Chatbubble: handlePreviousResponse: Error: llm not found in responses")
+      return <ActivityIndicator/>
+    }
+    const nextIndex = (curIndex + 1) % responses.length;
+    setLLM(responses[nextIndex][0]);
   }
 
   const handlePreviousResponse = () => {
-    const prevIndex = (responseIndex -1 + responses.length ) % Object.entries(message).length;
-    setResponseIndex(prevIndex);
+    const curIndex = responses.findIndex(([key, value]) => key === llm);
+    if(curIndex === -1) {
+      console.log("Chatbubble: handlePreviousResponse: Error: llm not found in responses")
+      return <ActivityIndicator/>
+    }
+    const prevIndex = (curIndex - 1 + responses.length) % responses.length;
+    setLLM(responses[prevIndex][0])
   }
 
+  //---------------Render Chat Bubble----------------
 
+  if(isUser) {
+    return userBubble();
+  }
+  // LLM --> Display Side by side chat bubbles
+  // biome-ignore lint/style/noUselessElse: <explanation>
+  else {
+    return llmBubble();
+  }
+
+  //---------------Subcomponents----------------
   function userBubble() {
     const text = message.user;
     return(
@@ -102,82 +129,5 @@ export function ChatBubble ({message} : ChatBubbleProps) {
       </View>
     );
   }
-
-  if(isUser) {
-    return userBubble();
-  }
-  // LLM --> Display Side by side chat bubbles
-  // biome-ignore lint/style/noUselessElse: <explanation>
-  else {
-    return llmBubble();
-  }
-
-
-
-  // return user chat bubble
-  // if(numberOfKeys === 1) {
-    // <View
-    //     key={key}
-    //     style={[
-    //       styles.message,
-    //       index % 2 === 0
-    //         ? [styles.sentMessage, { backgroundColor: colors.inversePrimary }]
-    //         : [styles.receivedMessage, { backgroundColor: colors.surfaceVariant }]
-    //     ]}
-    //   >
-    //     {index % 2 !== 1 && (
-    //       <IconButton
-    //         icon='volume-up'
-    //         size={16}
-    //         onPress={() =>
-    //           Speech.speak(message.text = {
-    //             language: 'en-US',
-    //             pitch: 1,
-    //             rate: 1
-    //           })
-    //         }
-    //         style={styles.speakButton}
-    //       />
-    //     )}
-    //     <Text>{message.text}</Text>
-    //   </View>
-  // }
-  // else {
-  //   <ScrollView horizontal style = {styles.container} > 
-  //       {responses.map((response, index) => (
-  //         <View key ={response} style = {[styles.chatContainer, {backgroundColor: colors.surface}]}>
-  //           <Text style = {styles.input}>{response}</Text>
-  //           </View>
-  //       ))}
-  //     </ScrollView>
-  // }
-
-  // <View
-      //   key={}
-      //   style={[
-      //     styles.message,
-      //     index % 2 === 0
-      //       ? [styles.sentMessage, { backgroundColor: colors.inversePrimary }]
-      //       : [styles.receivedMessage, { backgroundColor: colors.surfaceVariant }]
-      //   ]}
-      // >
-      //   {index % 2 !== 1 && (
-      //     <IconButton
-      //       icon='volume-up'
-      //       size={16}
-      //       onPress={() =>
-      //         Speech.speak(message.text = {
-      //           language: 'en-US',
-      //           pitch: 1,
-      //           rate: 1
-      //         })
-      //       }
-      //       style={styles.speakButton}
-      //     />
-      //   )}
-      //   <Text>{message.text}</Text>
-      // </View>
-
-  
 }
 
