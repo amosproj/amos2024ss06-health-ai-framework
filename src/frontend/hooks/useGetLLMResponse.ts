fd568eb5b5d47b37d27ba97391792d75e6ec18b6
Eloi Sandt <eloi.sandt@campus.tu-berlin.de>
2024-07-06 17:39:08 +0200

    feat: add getLLMResponse hook
    Co-authored-by: David Enescu <david.enescu@fau.de>, Lukas Varga <lukas.varga128@mail.com>
    Signed-off-by: Eloi Sandt <eloi.sandt@campus.tu-berlin.de>

---
+++ src/frontend/hooks/useGetLLMResponse.ts
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import type { conversationMessage } from '../types';
+import { exec } from 'node:child_process';
+import path from 'node:path';
+
+export function useGetLLMResponse(model: string, conversation: conversationMessage) {
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [LLMResponse, setLLMResponse] = useState<string | undefined>(undefined);
+  const [LLMResponseError, setError] = useState<string | undefined>(undefined);
+
+  switch(model) {
+    case 'gpt-4':
+      //TODO:
+      break;
+    case 'google':
+      //TODO:
+      break;
+    case 'mistral':
+      //TODO:
+      break;
+    case 'claude':
+      //TODO:
+      break;
+    default:
+      setError(`Error: model ${model} does not exist.`);
+  }
+
+  //TODO: execute python retriever script
+  const inputString = "What should I eat for lunch?";
+  const projectRoot = path.resolve(__dirname, "..", "..");
+  const retrieverFilePath = path.resolve(projectRoot, 'backend', 'RAG', 'LangChain_Implementation', 'retriever.py');
+
+  console.log("retriever file path", retrieverFilePath);
+
+  exec(`python ${retrieverFilePath} "${inputString}"`, (error, stdout, stderr) => {
+      if (error) {
+          console.error(`Error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.error(`Stderr: ${stderr}`);
+          return;
+      }
+      console.log(`Output: ${stdout}`);
+  });
+
+  //Delete this line once implemented connection to python backend
+  setIsGenerating(false);
+  setLLMResponse(`Hello I am ${model} and I cannot help you. (TODO: implement LLM backend connection)`);
+
+  return { LLMResponse, isGenerating, LLMResponseError };
+};
