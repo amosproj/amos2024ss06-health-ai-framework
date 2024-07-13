2987be519fef63f18f109df2446b200ff6b3f12b
eloinoel <eloi.sandt.games@gmx.de>
2024-07-13 13:02:42 +0200

    feat: Filter out any links from voice button read text
    Co-authored-by: Lukas Varga <lukas.varga128@gmail.com>
    Signed-off-by: eloinoel <eloi.sandt.games@gmx.de>

--- src/frontend/components/SpeakButton/index.tsx
+++ src/frontend/components/SpeakButton/index.tsx
@@ -1,25 +1,49 @@
 import { Style } from './style';
 import { IconButton } from 'react-native-paper';
 import * as Speech from 'expo-speech';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function SpeakButton(props: { response: string }) {
   const [isSpeaking, setIsSpeaking] = useState(false);
+
+  // Function to remove links from the text
+  function extractReadableText(response: string): string {
+    // Regular expression to match http and https links
+    const linkRegex = /(https?:\/\/[^\s]+)|(www\.[^\s]+)/g;
+    // Remove links from the text
+    return response.replace(linkRegex, '');
+  }
+
+  useEffect(() => {
+    return () => {
+      Speech.stop();
+    };
+  }, []);
+
+  // if button is pressed
+  const handleSpeech = () => {
+    if (isSpeaking) {
+      setIsSpeaking(false);
+      Speech.stop();
+    } else {
+      setIsSpeaking(true);
+      const readableText = extractReadableText(props.response);
+      console.log(readableText);
+      Speech.speak(readableText, {
+        language: 'en-US',
+        pitch: 1,
+        rate: 1,
+        onDone: () => setIsSpeaking(false),
+        onError: () => setIsSpeaking(false),
+      });
+    }
+  };
 
   return (
     <IconButton
       icon= {!isSpeaking ? 'volume-up' : 'volume-mute'}
       size={16}
-      onPress={() => {
-        if(isSpeaking) {
-          setIsSpeaking(false);
-          Speech.stop();
-        } else {
-          setIsSpeaking(true);
-          Speech.speak(props.response ? props.response : '',
-              { language: 'en-US', pitch: 1, rate: 1 });
-        }
-      }}
+      onPress={handleSpeech}
       style={Style.speakButton}
     />
   )
