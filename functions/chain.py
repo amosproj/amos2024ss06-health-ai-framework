a199ffcc71ab0e9b5ab17443ed8322c9ec442b1a
David Enescu <david.enescu@fau.de>
2024-07-15 15:20:46 +0200

    Created the connection to llm Co-authored-by: potthoffjan <potthoffjan@users.noreply.github.com>

--- functions/chain.py
+++ functions/chain.py
@@ -3,8 +3,8 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from meta import document_content_description, metadata_field_info
-
-
+from main import get_custom_instructions_callable
+print("custom instruction: " + get_custom_instructions_callable())
 def create_health_ai_chain(llm, vector_store):
     retriever = SelfQueryRetriever.from_llm(
         llm=llm,
@@ -13,6 +13,8 @@ def create_health_ai_chain(llm, vector_store):
         metadata_field_info=metadata_field_info,
         document_contents='',
     )
+    custom_istructions = get_custom_instructions_callable()
+    print("custom instruction: " + get_custom_instructions_callable())
     health_ai_template = """
     You are a health AI agent equipped with access to diverse sources of health data,
     including research articles, nutritional information, medical archives, and more.
@@ -20,7 +22,18 @@ def create_health_ai_chain(llm, vector_store):
     If you cannot find relevant information, simply state that you do not have enough data
     to answer accurately. write your response in markdown form and also add reference url
     so user can know from which source you are answering the questions.
+    """
+    user_info = " "
+    if custom_istructions:
+        user_info = f"""Here is some information about the user, including the user's name, 
+        his profile description and his style instructions on how you should answer: 
+        User Name: {custom_istructions['name']}
+        Style Instrctions: {custom_istructions['styleInstructions']}
+        Personal Info: {custom_istructions['personalInstructions']}
+        """
+    
 
+    context_string ="""
     CONTEXT:
     {context}
 
@@ -28,7 +41,10 @@ def create_health_ai_chain(llm, vector_store):
 
     YOUR ANSWER:
     """
-    health_ai_prompt = ChatPromptTemplate.from_template(health_ai_template)
+    
+    prompt_to_pass = health_ai_template + user_info + context_string
+
+    health_ai_prompt = ChatPromptTemplate.from_template(prompt_to_pass)
     chain = (
         {'context': retriever, 'question': RunnablePassthrough()}
         | health_ai_prompt
