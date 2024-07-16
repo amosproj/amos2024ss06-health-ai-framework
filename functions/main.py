b1844d5d6832c62dbfda1034b1ba34950500a60e
Preet Vadaliya <preetvadaliya@gmail.com>
2024-07-17 01:21:48 +0200

    chore: Update firebase functions with increased memory and timeout settings

--- functions/main.py
+++ functions/main.py
@@ -5,7 +5,7 @@ from chain import hist_aware_answers
 from firebase_functions import https_fn, options
 
 
-@https_fn.on_request()
+@https_fn.on_request(memory=options.MemoryOption.GB_32, cpu=8, timeout_sec=540)
 def get_response_url(req: https_fn.Request) -> https_fn.Response:
     query = req.get_json().get('query', '')
     llms = req.get_json().get('llms', ['gpt-4', 'gemini', 'claude'])
@@ -16,7 +16,7 @@ def get_response_url(req: https_fn.Request) -> https_fn.Response:
     return https_fn.Response(dumps(responses), mimetype='application/json')
 
 
-@https_fn.on_call(memory=options.MemoryOption.GB_32, cpu=8, timeout_sec=300)
+@https_fn.on_call(memory=options.MemoryOption.GB_32, cpu=8, timeout_sec=540)
 def get_response(req: https_fn.CallableRequest):
     query = req.data.get('query', '')
     llms = req.get_json().get('llms', ['gpt-4', 'gemini', 'claude'])
